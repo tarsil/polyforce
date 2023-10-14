c3c98982ed6a54c3c8f7945ea14fa4415198f484
tarsil <tiago.arasilva@gmail.com>
2023-10-14 16:26:41 +0100

    Add more tests to field and defaults

--- tests/fields/test_fields.py
+++ tests/fields/test_fields.py
@@ -1,6 +1,9 @@
 from typing import Any, Dict, List, Mapping, Union
 
+import pytest
+
 from polyforce import PolyField, PolyModel
+from polyforce.exceptions import ValidationError
 
 
 class Model(PolyModel):
@@ -8,13 +11,13 @@ class Model(PolyModel):
         ...
 
     def create_model(self, names: List[str]) -> None:
-        ...
+        return names
 
     def get_model(self, models: Dict[str, Any]) -> Dict[str, Any]:
-        ...
+        return models
 
     def set_model(self, models: Mapping[str, PolyModel]) -> None:
-        ...
+        return models
 
 
 def test_can_create_polyfield():
@@ -22,6 +25,52 @@ def test_can_create_polyfield():
     assert field is not None
     assert field.annotation == str
     assert field.name == "field"
+    assert field.is_required() is True
+
+
+def test_raise_type_error_on_default_field():
+    with pytest.raises(TypeError) as raised:
+        PolyField(annotation=str, default=2, name="name")
+
+    assert (
+        raised.value.args[0]
+        == "default 'int' for field 'name' is not valid for the field type annotation, it must be type 'str'"
+    )
+
+
+def test_default_field():
+    default = "john"
+
+    def get_default():
+        nonlocal default
+        return default
+
+    field = PolyField(annotation=str, default=get_default, name="name")
+    assert field.default == default
+
+
+def test_functions():
+    model = Model(name="PolyModel", age=1)
+
+    names = model.create_model(names=["poly"])
+    assert names == ["poly"]
+
+    models = model.get_model(models={"name": "poly"})
+    assert models == {"name": "poly"}
+
+    models = model.set_model(models={"name": "poly"})
+    assert models == {"name": "poly"}
+
+
+@pytest.mark.parametrize("func", ["get_model", "set_model"])
+def test_functions_raises_validation_error(func):
+    model = Model(name="PolyModel", age=1)
+
+    with pytest.raises(ValidationError):
+        model.create_model(names="a")
+
+    with pytest.raises(ValidationError):
+        getattr(model, func)(models="a")
 
 
 def test_poly_fields():
